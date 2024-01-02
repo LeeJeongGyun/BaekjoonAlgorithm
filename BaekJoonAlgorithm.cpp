ea2d453cb6a2b3c0fa8c9554289e5a1b7973b18e
JKLEE <jeonggyun0630@gmail.com>
2024-01-02 22:00:36 +0900

    10808 알파벳 개수 - 전역변수 사용하는 버전으로 수정

--- BaekJoonAlgorithm.cpp
+++ BaekJoonAlgorithm.cpp
@@ -2,22 +2,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string s;
+int cnt[26];
+
 int main()
 {
-	char str[101];
-	::memset(str, 0, sizeof(str));
-	cin >> str;
-
-	int ans[26];
-	::memset(ans, 0, sizeof(ans));
-	for (int i = 0; str[i] != '\0'; ++i)
+	cin >> s;
+	for (int i = 0; i < s.length(); ++i)
 	{
-		ans[str[i] - 'a']++;
+		cnt[s[i] - 'a']++;
 	}
-	
+
 	for (int i = 0; i < 26; ++i)
 	{
-		cout << ans[i] << " ";
+		cout << cnt[i] << " ";
 	}
 
 	return 0;
