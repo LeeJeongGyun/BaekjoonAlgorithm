c2fe74f326c0f5f9555cd8eaee2fb4d1767d4faa
JKLEE <jeonggyun0630@gmail.com>
2024-01-02 21:37:26 +0900

    백준 10808 알파벳 개수

---
+++ BaekJoonAlgorithm.cpp
@@ -0,0 +1,24 @@
+﻿
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	char str[101];
+	::memset(str, 0, sizeof(str));
+	cin >> str;
+
+	int ans[26];
+	::memset(ans, 0, sizeof(ans));
+	for (int i = 0; str[i] != '\0'; ++i)
+	{
+		ans[str[i] - 'a']++;
+	}
+	
+	for (int i = 0; i < 26; ++i)
+	{
+		cout << ans[i] << " ";
+	}
+
+	return 0;
+}
