2f8ac62fd067aa804dc01505c22f15c8d8b82fe5
JKLEE <jeonggyun0630@gmail.com>
2024-01-08 22:01:53 +0900

    백준 9375 (패션왕 신해빈) : 1 - J - 경우의 수 (long long) - 아무것도 입지 않는 것을 포함해서 경우의 수 도출

--- BaekJoonAlgorithm.cpp
+++ BaekJoonAlgorithm.cpp
@@ -2,35 +2,32 @@
 using namespace std;
 
 int n, m;
-map<string, int> m1;
-map<int, string> m2;
-
+string input1, input2;
 int main() 
 {
 	std::ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> m;
-	string s;
-	for (int i = 1; i <= n; ++i)
+	cin >> n;
+	for (int i = 0; i < n; ++i)
 	{
-		cin >> s;
-		m1[s] = i;
-		m2[i] = s;
+		map<string, int> gmap;
+		long long ans = 1;
+		cin >> m;
+		for (int i = 0; i < m; ++i)
+		{
+			cin >> input1 >> input2;
+
+			gmap[input2]++;
+		}
+
+		for (auto a : gmap)
+		{
+			ans *= (long long)(a.second + 1);
+		}
+		--ans;
+		cout << ans << endl;
 	}
 
-	for (int i = 0; i < m; ++i)
-	{
-		cin >> s;
-
-		if (atoi(s.c_str()) != 0)
-		{
-			cout << m2[atoi(s.c_str())] << endl;
-		}
-		else
-		{
-			cout << m1[s] << endl;
-		}
-	}
 	return 0;
 }
