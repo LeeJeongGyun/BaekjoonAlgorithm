29f4d4692bc0c224f7d56eec5efe760d3d4cd08b
JKLEE <jeonggyun0630@gmail.com>
2024-02-05 21:25:12 +0900

    백준 2468(안전 영역) 2 - C { 좀 더 효율적인 풀이 - 높이를 인자로 전달하는 방식 }

--- BaekJoonAlgorithm.cpp
+++ BaekJoonAlgorithm.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 int n;
 int ori[104][104];
-int cop[104][104];
+int check[104][104];
 int minH = 101, maxH = 0;
 int num, ans;
 
 int dx[] = { 0,1,0,-1 };
 int dy[] = { -1,0,1,0 };
 
-void DFS(int y, int x)
+void DFS(int y, int x, int h)
 {
-	cop[y][x] = 0;
+	check[y][x] = 1;
 	for (int i = 0; i < 4; ++i)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
-		if (ny < 0 || ny >= n || nx < 0 || nx >= n || cop[ny][nx] == 0) continue;
-		DFS(ny, nx);
+		if (ny < 0 || ny >= n || nx < 0 || nx >= n || ori[ny][nx] <= h || check[ny][nx] != 0) continue;
+		DFS(ny, nx, h);
 	}
 }
 
@@ -39,23 +39,16 @@ int main()
 
 	for (int h = minH - 1; h <= maxH; ++h)
 	{
-		memcpy(cop, ori, sizeof(ori));
-		for (int i = 0; i < n; ++i)
-		{
-			for (int j = 0; j < n; ++j)
-			{
-				if (cop[i][j] <= h) cop[i][j] = 0;
-			}
-		}
+		memset(check, 0, sizeof(check));
 
 		int total = 0;
 		for (int i = 0; i < n; ++i)
 		{
 			for (int j = 0; j < n; ++j)
 			{
-				if (cop[i][j] != 0)
+				if (ori[i][j] > h && check[i][j] == 0)
 				{
-					DFS(i, j);
+					DFS(i, j, h);
 					total++;
 				}
 			}
