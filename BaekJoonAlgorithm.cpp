8de6c54df981fa6a931c4e5a6f6788b35ceea582
JKLEE <jeonggyun0630@gmail.com>
2024-02-05 21:16:40 +0900

    백준 2468(안전 영역) 2 - C { minH(제일 낮은 높이) - 1 부터 검사해서 전부 다 잠기지 않는 경우를 고려 }

--- BaekJoonAlgorithm.cpp
+++ BaekJoonAlgorithm.cpp
@@ -2,54 +2,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t;
-int m, n, k, x, y;
-int gmap[54][54];
-int ans;
+int n;
+int ori[104][104];
+int cop[104][104];
+int minH = 101, maxH = 0;
+int num, ans;
+
 int dx[] = { 0,1,0,-1 };
 int dy[] = { -1,0,1,0 };
 
 void DFS(int y, int x)
 {
+	cop[y][x] = 0;
 	for (int i = 0; i < 4; ++i)
 	{
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
-		if (ny < 0 || ny >= n || nx < 0 || nx >= m || gmap[ny][nx] == 0) continue;
-		gmap[ny][nx] = 0;
+		if (ny < 0 || ny >= n || nx < 0 || nx >= n || cop[ny][nx] == 0) continue;
 		DFS(ny, nx);
 	}
 }
 
 int main()
 {
-	cin >> t;
-	for (int i = 0; i < t; ++i)
+	cin >> n;
+	for (int i = 0; i < n; ++i)
 	{
-		cin >> m >> n >> k;
-		memset(gmap, 0, sizeof(gmap));
-		ans = 0;
-		for (int j = 0; j < k; ++j)
+		for (int j = 0; j < n; ++j)
 		{
-			cin >> x >> y;
-			gmap[y][x] = 1;
+			cin >> ori[i][j];
+			maxH = max(maxH, ori[i][j]);
+			minH = min(minH, ori[i][j]);
+		}
+	}
+
+	for (int h = minH - 1; h <= maxH; ++h)
+	{
+		memcpy(cop, ori, sizeof(ori));
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+			{
+				if (cop[i][j] <= h) cop[i][j] = 0;
+			}
 		}
 
-		for (int y = 0; y < n; ++y)
+		int total = 0;
+		for (int i = 0; i < n; ++i)
 		{
-			for (int x = 0; x < m; ++x)
+			for (int j = 0; j < n; ++j)
 			{
-				if (gmap[y][x] == 1)
+				if (cop[i][j] != 0)
 				{
-					gmap[y][x] = 0;
-					DFS(y, x);
-					ans++;
+					DFS(i, j);
+					total++;
 				}
 			}
 		}
-		
-		cout << ans << endl;
+
+		ans = max(ans, total);
 	}
+
+	cout << ans << endl;
+
 	return 0;
 }
